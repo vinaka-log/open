969e385cc32a5206ac3680d2bf9bff69a61072aa
vinaka-log <k64.yuta29@gmail.com>
2023-03-25 19:05:38 +0900

    4

--- react-tran/react-tutorial/src/Form.js
+++ react-tran/react-tutorial/src/Form.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+
+    const [text, setText] = useState('');
+
+    const submitForm = (event) => {
+        // ファームをサブミットするとページ遷移してしまうがそれを止めるため。
+        event.preventDefault();
+        console.log(text);
+    }
     return (
-        <div>Form</div>
+        <div>
+            <h4>新しい言語の追加</h4>
+            <form onSubmit={submitForm}>
+                <div>
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)} />
+                </div>
+                <div>
+                    <button>追加</button>
+                </div>
+            </form>
+        </div>
     )
 }
 
